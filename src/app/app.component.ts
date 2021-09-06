33f32813f5fe9b2852857f16cd11ff22d31a1aae
Redouane Bekkouche <Redouane Bekkouche>
2021-09-06 22:38:23 +0100

    sep 06 2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Todo } from './models/todo';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'toDo-app';
+export class AppComponent implements OnInit {
+
+  todos: Todo[];
+  InputTodo: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [];
+  }
+
+  doneTask(id) {
+    this.todos.map(
+      (value, i) => {
+        if (i === id) {
+          value.completed = !value.completed;
+        }
+      }
+    )
+  }
+
+  deleteTask(i) {
+    this.todos.splice(i, 1);
+  }
+
+  addTask() {
+    if (this.InputTodo.length !== 0) {
+      this.todos.push(
+        { content: this.InputTodo, completed: false }
+      );
+      this.InputTodo = '';
+    }
+  }
+
 }
